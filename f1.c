03f63e56c35734028bff13fd66d761de86a8fdcd
Diksha-Kamble22 <dikshakamble216@gmail.com>
2022-11-05 13:41:34 +0530

    commit Message!

---
+++ f1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+ int x , y;
+ printf("\n enter the values of x and y ");
+ scanf ("%d " ,&x );
+ y = x*x;
+ printf ("\n solution is =%d ",y);
+ return 0;
+}
